bea2f76d0ed876dc4a68e735adfb10220e793c80
Taylor Anderson <taylorranderson@gmail.com>
2021-07-20 16:43:08 -0700

    timeblocks now change color based on time

--- docs/jss/script.js
+++ docs/jss/script.js
@@ -3,3 +3,30 @@
 var currentDay = moment().format("dddd, MMMM, Do");
 $("#currentDay").text(currentDay);
 
+// function to set color based on if hour is in past present or future
+
+function setHour() {
+    //set current hour (military time)
+    var currentHour = moment().hour();
+
+        // Loop through grid rows based on id
+        $(".time-block").each(function() {
+
+            // assign each row an hour based on id (military time)
+            var rowHour = parseInt($(this).attr("id"));
+
+            // compare row hour to current hour and assign class
+            if (currentHour > rowHour) {
+                $(this).addClass("past");
+            }
+            else if (currentHour < rowHour) {
+                $(this).addClass("future");
+            }
+            else if (currentHour === rowHour) {
+                $(this).addClass("present");
+            }
+        })
+    }
+
+// call function 
+setHour();
